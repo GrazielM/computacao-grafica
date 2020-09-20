9107683eb70321f9e2cf1a967c1143a66ea3a88d
Graziel Medeiros <graziel.medeiros@gmail.com>
2020-09-20 16:48:23 -0300

    Initial Commit

---
+++ src/main/java/br/com/anhembi/cco/computacaografica/aula2/input/KeyBoard.java
@@ -0,0 +1,31 @@
+package br.com.anhembi.cco.computacaografica.aula2.input;
+
+import br.com.anhembi.cco.computacaografica.aula2.cena.Cena;
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+
+/**
+ * @author Siabreu
+ */
+public class KeyBoard implements KeyListener {
+  private Cena cena;
+
+  public KeyBoard(Cena cena) {
+    this.cena = cena;
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    System.out.println("Key pressed: " + e.getKeyCode());
+    if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
+      System.exit(0);
+
+    if (e.getKeyChar() == 'a')
+      System.out.println("Pressionou tecla a");
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+  }
+
+}
---
+++ src/test/java/br/com/anhembi/cco/computacaografica/ComputacaoGraficaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anhembi.cco.computacaografica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputacaoGraficaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
